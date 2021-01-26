8ceb3b442eb6e3179de8f3dcc17aa609f7fe1a39
anabrkic <anabrkic1311@gmail.com>
2021-01-26 22:00:08 +0100

    fix footer positioning

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => {
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center p-5">
+        <div className="d-flex flex-column justify-content-center align-items-center p-5" style={{ position: 'absolute', bottom: 0, marginLeft: -200, width: 400, left: '50%' }}>
             <div>Zavrsni rad</div>
             <div>Sveucilisni odjel za strucne studije</div>
             <div>Copyright Ana Brkic</div>
